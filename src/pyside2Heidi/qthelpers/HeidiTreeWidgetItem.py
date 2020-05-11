7284e6505548aff0b7f01c2874ecbf6777f36631
xcf007 <xcf-007@163.com>
2020-05-11 23:51:32 +0800

    qthelper

---
+++ src/pyside2Heidi/qthelpers/HeidiTreeWidgetItem.py
@@ -0,0 +1,7 @@
+from PySide2.QtWidgets import QTreeWidgetItem
+
+class HeidiTreeWidgetItem(QTreeWidgetItem):
+    """
+    @type itemType: str
+    """
+    itemType = ''
---
+++ src/pyside2Heidi/qthelpers/helper_methods.py
@@ -0,0 +1,9 @@
+def buildQStringList(items):
+    """
+    @rtype: QStringList
+    """
+    returnData = []
+    for item in items:
+        returnData.append(item)
+
+    return returnData
